3c6fa24a36204bb11d44d50005f60b35da38a32a
kvg621 <konst000@yandex.ru>
2021-11-10 17:49:07 +0400

    add Button

--- app/src/main/java/com/example/transactivity/CityViewHolder.kt
+++ app/src/main/java/com/example/transactivity/CityViewHolder.kt
@@ -1,15 +1,20 @@
 package com.example.transactivity
 
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class CityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(cityName: String) {
-val cityNameTextView:TextView=itemView.findViewById(R.id.textView_item)
-     cityNameTextView.text=cityName
-        itemView.setOnClickListener(){
+        val cityNameTextView: TextView = itemView.findViewById(R.id.textView_item)
+        val toastButton: Button = itemView.findViewById(R.id.toast_bottom)
+        toastButton.setOnClickListener(){
+            Toast.makeText(itemView.context, "Выбран город: ${cityName}", Toast.LENGTH_SHORT).show()
+        }
+        cityNameTextView.text = cityName
+        itemView.setOnClickListener() {
             Toast.makeText(itemView.context, cityName, Toast.LENGTH_SHORT).show()
         }
     }
